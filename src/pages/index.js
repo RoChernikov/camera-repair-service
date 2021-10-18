af23c623930c3177f8433e789c64048c7fc837ac
Ro Chernikov <imrhubris@gmail.com>
2021-10-18 16:06:14 +0300

    feat: webpack infrastructure is done

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+// import '../pages/index.css';
+import '../pages/index.scss';
+// ТЕСТОВЫЙ КОД-------------------------------------------
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
+console.log('It Works!');
+console.log(doubledNumbers); // 4, 6, 10
+// -------------------------------------------------------
